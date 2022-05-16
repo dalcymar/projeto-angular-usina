66f1c2ad574b016f10d72964824c3b391718b684
Dalcimar Luiz <dalcymar.oliveira@gmail.com>
2022-05-16 12:11:19 -0300

    Criado o rodape para uso geral das paginas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { ContatoComponent } from './pages/contato/contato.component';
+import { RodapeComponent } from './rodape/rodape.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    ContatoComponent
+    ContatoComponent,
+    RodapeComponent
   ],
   imports: [
     BrowserModule,
