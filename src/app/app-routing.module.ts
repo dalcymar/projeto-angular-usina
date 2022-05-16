241db1254aa7c9d23beafb7123644432cfc30ca6
Dalcimar Luiz <dalcymar.oliveira@gmail.com>
2022-05-16 10:14:20 -0300

    First commit do projeto angular DIO

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContatoComponent } from './pages/contato/contato.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'contato',
+    component: ContatoComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
